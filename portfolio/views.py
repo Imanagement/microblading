006029e0950f7d96b99ab0adfe47ed9b41edf3b4
vivi <vivi@anruss-iMac.local>
2021-11-18 14:28:06 +0200

    Minifying the static files

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,6 +8,7 @@ class PortfolioListView(ListView):
     model = PortfolioItem
     paginate_by = 20
     template_name = 'portfolio/pages/portfolio.html'
+    context_object_name = 'portfolio_items'
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
