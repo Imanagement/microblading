adb238451885cecc7623c7d86efdf2640fcaeead
artyomkoltunov <test@test.test>
2022-07-27 00:24:34 +0300

    title, meta, and page redirecting between 2 languages

--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,5 @@ from services.views import service_list, service_detail
 app_name = 'services'
 urlpatterns = [
     path('', service_list, name="service-list"),
-    path('<slug>', service_detail, name="service-detail"),
+    path('<slug>/', service_detail, name="service-detail"),
 ]
